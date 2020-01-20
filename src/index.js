734c56a5953a3c63894014ed63cf27c3a5992f3a
Артем Друщиц <temafm-3@yandex.ru>
2020-01-20 21:05:06 +0300

    feat: added priority

--- src/index.js
+++ src/index.js
@@ -17,13 +17,16 @@ import './index.css';
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       }
     }
     handleClick(i) {
       const squares = this.state.squares.slice();
-      console.log(squares);
-      squares[i] = 'X';
-      this.setState({squares: squares})
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      })
     }
     renderSquare(i) {
       return <Square 
@@ -32,7 +35,7 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
